d31a879cea7e0aab9c2887ee702fa2dff29a71c4
bhrzali <bhrz.ali@gmail.com>
2017-01-04 17:59:38 +0430

    q13 q 14

---
+++ Chapter 3 Linear Regression/Q14/q14.r
@@ -0,0 +1,46 @@
+
+set.seed(1)
+x1 = runif(100)
+x2 = 0.5*x1+rnorm(100)/10
+y = 2+2*x1+0.3*x2+rnorm(100)
+
+cor(x1,x2)
+plot(x1,x2)
+
+lm.fit1 = lm(y~x1+x2)
+summary(lm.fit1)
+
+lm.fit2 = lm(y~x1)
+summary(lm.fit2)
+
+lm.fit3 = lm(y~x2)
+summary(lm.fit3)
+
+x1 = c(x1, 0.1)
+x2 = c(x2, 0.8)
+y = c(y,6)
+
+lm.fit_g = lm(y~x1+x2)
+summary(lm.fit_g)
+par(mfrow=c(2,2))
+plot(lm.fit_g)
+
+p=2
+n = length(y)
+(p+1)/n
+
+lm.fit_g2 = lm(y~x1)
+summary(lm.fit_g2)
+par(mfrow=c(2,2))
+plot(lm.fit_g2)
+par(mfrow=c(1,1))
+plot(predict(lm.fit_g2),rstudent(lm.fit_g2))
+
+lm.fit_g3 = lm(y~x2)
+summary(lm.fit_g3)
+par(mfrow=c(2,2))
+plot(lm.fit_g3)
+par(mfrow=c(1,1))
+plot(predict(lm.fit_g3),rstudent(lm.fit_g3))
+
+
