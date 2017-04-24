c03ad5070ca866c11a4dc2513f22c16c7e64090c
bhrzali <bhrz.ali@gmail.com>
2017-04-24 12:38:39 +0430

    chap7 q11

--- Chapter 7 Moving Beyond Linearity/Q11/q11.r
+++ Chapter 7 Moving Beyond Linearity/Q11/q11.r
@@ -54,30 +54,3 @@ abline(lm.model$coef[1],0,col="red",lty=2)
 abline(lm.model$coef[2],0,col="blue",lty=2)
 abline(lm.model$coef[3],0,col="green",lty=2)
 legend(0,10,legend=c("b0_hat","b1_hat","b2_hat"),col=c("red","blue","green"),lty=1, cex=0.8)
-
-b1_hat = 6
-b0_hat_lst = c()
-b1_hat_lst = c()
-b2_hat_lst = c()
-
-for(i in 1:2){
-    m1 = y-b1_hat*x1
-    model = lm(m1~x2)
-    b2_hat = model$coef[2]
-    
-    m2 = y-b2_hat*x2
-    model = lm(m2~x1)
-    b1_hat = model$coef[2]
-    
-    b0_hat_lst[i]=model$coef[1]
-    b1_hat_lst[i]=b1_hat
-    b2_hat_lst[i]=b2_hat
-}
-
-b0_hat_lst[2]
-
-b1_hat_lst[2]
-
-b2_hat_lst[2]
-
-
